77a7dd025e6bf8534cecdd066ff47fc7d292f6ed
FelipeB82 <felipebalmaceda@gmail.com>
2023-01-02 18:55:43 -0300

    login solucionado
    Se corrigio error en auth.service.ts

--- FrontEnd/src/app/service/auth.service.ts
+++ FrontEnd/src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'httP://localhost:8080';
+  authURL = 'httP://localhost:8080/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
