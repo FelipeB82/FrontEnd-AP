45c02bf8f2223c8ecfb56fffac6eb2161547f22d
Luis Felipe Balmaceda <105952919+FelipeB82@users.noreply.github.com>
2023-01-12 19:58:28 -0300

    deploy firebase

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'httP://localhost:8080/auth/';
+  authURL = 'https://porfolio-lfb.onrender.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    URL: ''
+    URL: 'https://porfolio-lfb.onrender.com'
   };
